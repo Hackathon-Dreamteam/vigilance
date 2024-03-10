a309089ee6408b340e5a19145a390a7745f096a0
Mathieu Gamache <mathieu.gamache@workleap.com>
2024-03-10 12:02:48 -0400

    Add species controller

--- backend/InvasionQc.Api/Controllers/AlertsController.cs
+++ backend/InvasionQc.Api/Controllers/AlertsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace InvasionQc.Api.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/alerts")]
 public class AlertsController: ControllerBase
 {
     private readonly IMediator _mediator;
